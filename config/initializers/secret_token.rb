06b7cd334b2db3d2c7cd7c4089403a2d28d4e57d
Joseph Mastey <jmmastey@gmail.com>
2014-04-13 14:14:43 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Learnenova::Application.config.secret_key_base = '0ddb015bf663adb1cdb2e46c3cd81df4ed6e950fea5f21f499f221dd3e2f29fb4aa4f14a7d4f6d999eec098becfa15e81201122a7055ef4c893849db7e0b4aa1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Learnenova::Application.config.session_store :cookie_store, key: '_learnenova_session'
