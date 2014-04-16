0079f091c371b89659a073a69d774a5755729429
Joseph Mastey <jmmastey@gmail.com>
2014-04-16 15:18:27 -0500

    move to module format

---
+++ app/controllers/modules_controller.rb
@@ -0,0 +1,2 @@
+class ModulesController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Learnenova::Application.routes.draw do
+  root to: "home#index"
+
   get "home/index"
-  root :to => "home#index"
-  devise_for :users, :controllers => {:registrations => "registrations"}
+  devise_for :users, controllers: {:registrations => "registrations"}
   resources :users
+  get "modules/:action", controller: "modules"
 end
