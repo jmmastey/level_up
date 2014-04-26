2c36b787ebda28e2366d05cf547d28234ac8f03c
Joseph Mastey <jmmastey@gmail.com>
2014-04-26 18:22:55 -0500

    more cleanup

--- spec/factories/verifiers.rb
+++ spec/factories/verifiers.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
 
   # sucks that this isn't just user...
   factory :verifier do
-    sequence(:email) { |n| "jmmastey+vfactory_#{n}@gmail.com" }
+    sequence(:email) { |n| "jmmastey+verif_factory_#{n}@gmail.com" }
 
     name 'Test User'
     password 'changeme'
