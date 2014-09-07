d2c8d4e9315701067df038942079de0af5ba1734
Joseph Mastey <jmmastey@gmail.com>
2014-09-07 16:49:40 -0500

    refactor to split summaries

--- app/models/summaries.rb
+++ app/models/summaries.rb
@@ -1,90 +1,13 @@
 module Summaries
   def self.for_user(user)
-    return {} if user.courses.empty?
-    output = user_map(user_summary_data(user))
-    filter_by_enrollment(output, user)
+    Summaries::UserSummary.new(user).to_h
   end
 
   def self.for_course(course, user)
-    summary = { total: 0, completed: 0, verified: 0 }
-    for_user(user).each do |category, stats|
-      next unless category_in_course?(course, category)
-
-      summary[:total]     += stats[:total_skills]
-      summary[:completed] += stats[:total_completed]
-      summary[:verified]  += stats[:total_verified]
-    end
-
-    summary
+    Summaries::CourseSummary.new(course, user).to_h
   end
 
   def self.for_category(user)
-    summary     = for_user(user)
-    categories  = Category.by_courses(user.courses).sorted
-
-    categories.map do |category|
-      summary[category.handle].merge(handle: category.handle)
-    end
-  end
-
-  protected
-
-  # in a long line of "forgive me" comments, fuck all of this.
-  # i'm exhausted, and at least this is well factored enough
-  # that I can pull it out later.
-  def self.filter_by_enrollment(summary_data, user)
-    categories = user.courses.map(&:categories).flatten.map(&:handle)
-    summary_data.select { |key, _| categories.include? key }
-  end
-
-  def self.category_in_course?(course, category)
-    course.categories.where(handle: category).any?
-  end
-
-  def self.user_summary_data(user)
-    connection.execute(summary_join(user).to_sql)
-  end
-
-  def self.summary_join(user)
-    summary_tables.project(summary_fields)
-        .project(Completion[:verified_on].count.as("total_verified"))
-        .project(Completion[:id].count.as("total_completed"))
-        .project(Skill[:id].count.as("total_skills"))
-      .group(summary_fields)
-      .order(:sort_order)
-  end
-
-  def self.summary_tables
-    Category
-      .joins(:skills)
-      .joins(ArelHelpers.join_association(Skill, :completions, Arel::OuterJoin)) do |_, conditions|
-        conditions.and(Completion[:user_id].eq(user.id))
-      end
-  end
-
-  def self.summary_fields
-    [Category[:id], Category[:name], Category[:handle]]
-  end
-
-  def self.user_map(summary)
-    summary = summary.map do |cat|
-      [cat['handle'], category_map(cat)]
-    end
-    Hash[summary]
-  end
-
-  def self.category_map(result)
-    {
-      id:               result['id'].to_i,
-      name:             result['name'],
-      handle:           result['handle'],
-      total_skills:     result['total_skills'].to_i,
-      total_completed:  result['total_completed'].to_i,
-      total_verified:   result['total_verified'].to_i,
-    }
-  end
-
-  def self.connection
-    ActiveRecord::Base.connection
+    Summaries::CategorySummary.new(user).to_h
   end
 end
---
+++ app/models/summaries/course_summary.rb
@@ -0,0 +1,25 @@
+module Summaries
+  class CourseSummary < Summaries::Base
+
+    def initialize(course, user)
+      @data = { total: 0, completed: 0, verified: 0 }
+      summarize_for(course, user)
+    end
+
+    private
+
+    def summarize_for(course, user)
+      Summaries.for_user(user).each do |category, stats|
+        next unless category_in_course?(course, category)
+
+        @data[:total]     += stats[:total_skills]
+        @data[:completed] += stats[:total_completed]
+        @data[:verified]  += stats[:total_verified]
+      end
+    end
+
+    def category_in_course?(course, category)
+      course.categories.where(handle: category).any?
+    end
+  end
+end
