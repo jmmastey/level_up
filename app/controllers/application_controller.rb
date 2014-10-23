c345ddcf7d56bfa8d14ad706a668053acabbe997
Joseph Mastey <jmmastey@gmail.com>
2014-10-23 13:09:07 -0500

    add miniprofiler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :miniprofiler
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, alert: exception.message
@@ -9,4 +10,10 @@ class ApplicationController < ActionController::Base
     super || Guest.new
   end
   helper_method :current_user
+
+  private
+
+  def miniprofiler
+    Rack::MiniProfiler.authorize_request if current_user.has_role? 'admin'
+  end
 end
