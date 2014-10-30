aebb6753045cc1b9dcb1f3506382091170283fd3
Joseph Mastey <jmmastey@gmail.com>
2014-10-29 20:49:51 -0500

    what could go wrong with testing in prod?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :miniprofiler
+  before_filter :redirect_to_real_domain
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, alert: exception.message
@@ -15,5 +16,11 @@ class ApplicationController < ActionController::Base
 
   def miniprofiler
     Rack::MiniProfiler.authorize_request if current_user.has_role? 'admin'
+  end
+
+  def redirect_to_real_domain
+    if request.host =~ /herokuapp./
+      redirect_to request.url.gsub(/herokuapp./, '')
+    end
   end
 end
