8ba4abecb2e99df80f2d7aa20a2ccef8d54ccb89
Joseph Mastey <jmmastey@gmail.com>
2014-10-30 10:15:21 -0500

    add some perf improvements and start fragment caching

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,7 @@ class Category < ActiveRecord::Base
   include ArelHelpers::ArelTable
 
   has_many :skills
-  has_many :courses, through: :skills
+  has_many :courses, -> { uniq }, through: :skills
 
   validates_presence_of :name, :handle
   validates_uniqueness_of :handle
@@ -10,5 +10,7 @@ class Category < ActiveRecord::Base
   default_scope -> { where(hidden: false) }
   scope :hidden, -> { where(hidden: true) }
   scope :sorted, -> { order(:sort_order) }
-  scope :by_courses, ->(c) { includes(:courses).where(id: c.map(&:id)) }
+  scope :by_courses, ->(c) do
+    includes(:courses).where(courses: {id: c.map(&:id)}).uniq
+  end
 end
