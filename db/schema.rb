993862901a23017d9b62a77b459bf853e8a5ad26
Joseph Mastey <jmmastey@gmail.com>
2014-10-31 11:03:13 -0500

    Initial model changes for difficulty.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141002161138) do
+ActiveRecord::Schema.define(version: 20141031154844) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20141002161138) do
     t.datetime "updated_at"
     t.integer  "sort_order"
     t.boolean  "hidden",                 default: true
+    t.integer  "difficulty"
   end
 
   create_table "completions", force: true do |t|
