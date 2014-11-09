c831837a62841d4bf919eae5d0f9750d9d840c3a
Joseph Mastey <jmmastey@gmail.com>
2014-11-09 13:37:16 -0600

    remove unused category summary

--- app/models/summaries.rb
+++ app/models/summaries.rb
@@ -6,8 +6,4 @@ module Summaries
   def self.for_course(course, user)
     Summaries::CourseSummary.new(course, user).to_h
   end
-
-  def self.for_category(user)
-    Summaries::CategorySummary.new(user).to_h
-  end
 end
