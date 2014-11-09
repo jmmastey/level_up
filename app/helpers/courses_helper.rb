a8e7c27b1e1d6a9418ec10179aa51132db300910
Joseph Mastey <jmmastey@gmail.com>
2014-11-09 17:17:17 -0600

    move logic out of views to stop setting locals

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,5 +1,12 @@
 module CoursesHelper
-  def completion_percent_for(summary)
+  def completion_summary_for(course, user = current_user)
+    summary = user.summary.for_course(course)
+    "#{summary[:completed]} of #{summary[:total]} skills completed"
+  end
+
+  def completion_percent_for(course, user = current_user)
+    summary = user.summary.for_course(course)
+
     return 0 unless summary[:total] > 0
     ((summary[:completed] / summary[:total].to_f) * 100).ceil
   end
