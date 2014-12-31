dba04660992487caa6723a36651d9b21959f5730
Joseph Mastey <jmmastey@gmail.com>
2014-12-31 15:42:11 -0600

    workaround issue with activerecord-session_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,13 @@
 # Be sure to restart your server when you modify this file.
+#
+# fuuuuck you deprecated gem
+module Kernel
+  def quietly_with_deprecation_silenced(&block)
+    ActiveSupport::Deprecation.silence do
+      quietly_without_deprecation_silenced(&block)
+    end
+  end
+  alias_method_chain :quietly, :deprecation_silenced
+end
 
 Levelup::Application.config.session_store :active_record_store
