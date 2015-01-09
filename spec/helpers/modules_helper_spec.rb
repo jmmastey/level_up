819cb9c8aea828b42e78d4563d5420db06ffd96e
Joseph Mastey <jmmastey@gmail.com>
2015-01-09 15:50:03 -0600

    prod doesn't love kwargs

--- spec/helpers/modules_helper_spec.rb
+++ spec/helpers/modules_helper_spec.rb
@@ -7,11 +7,11 @@ describe ModulesHelper, type: :helper do
   let(:user) { create(:user) }
   let(:guest) { Guest.new }
 
-  def enroll_user(created_at: Date.today)
+  def enroll_user(created_at = Date.today)
     create(:enrollment, user: user, course: course, created_at: created_at)
   end
 
-  def complete_skill(user = user, skill = skill, created_at: Date.today)
+  def complete_skill(created_at = Date.today, user = user, skill = skill)
     create(:completion, user: user, skill: skill, created_at: created_at)
   end
 
@@ -42,9 +42,9 @@ describe ModulesHelper, type: :helper do
     end
 
     it "returns false when the user completed the module" do
-      enroll_user(created_at: 3.weeks.ago)
+      enroll_user(3.weeks.ago)
       category.skills.each do |skill|
-        complete_skill(user, skill, created_at: 3.weeks.ago)
+        complete_skill(3.weeks.ago, user, skill)
       end
 
       stuck = helper.user_is_stuck?(user, category)
@@ -52,15 +52,15 @@ describe ModulesHelper, type: :helper do
     end
 
     it "returns true when enrollment is super old" do
-      enroll_user(created_at: 3.weeks.ago)
+      enroll_user(3.weeks.ago)
 
       stuck = helper.user_is_stuck?(user, category)
       expect(stuck).to be_truthy
     end
 
     it "returns true when last activity is way old" do
-      enroll_user(created_at: 3.weeks.ago)
-      complete_skill(created_at: 3.weeks.ago)
+      enroll_user(3.weeks.ago)
+      complete_skill(3.weeks.ago, user, skill)
 
       stuck = helper.user_is_stuck?(user, category)
       expect(stuck).to be_truthy
