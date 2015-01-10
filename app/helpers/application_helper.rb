808ee7813478e3e92bf3a1c8b823e5a094bef316
Joseph Mastey <jmmastey@gmail.com>
2015-01-09 18:13:38 -0600

    add wobble to feedback link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
   def admin_email
     ENV["ADMIN_EMAIL"] || ENV["GMAIL_USERNAME"] || "test@test.com"
   end
+
+  def feedback_link_classes
+    if session[:feedback_seen].present? && session[:feedback_seen] >= Date.today
+      "btn feedback-link"
+    else
+      session[:feedback_seen] = Date.today
+      "btn feedback-link wobble"
+    end
+  end
 end
