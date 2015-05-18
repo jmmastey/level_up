6640b2660b14d996ae748a023725d1e8fca573ad
Joseph Mastey <jmmastey@gmail.com>
2015-05-18 14:55:14 -0500

    change stuck message to point to slack

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Levelup::Application.configure do
   config.action_mailer.default_url_options = { host: ENV["DOMAIN_NAME"] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
