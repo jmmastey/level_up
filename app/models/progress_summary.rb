7dbee5bc56350c6eead41f7c6c8f86ea713c4d69
Joseph Mastey <jmmastey@gmail.com>
2015-05-18 10:34:59 -0500

    progress summary work

--- app/models/progress_summary.rb
+++ app/models/progress_summary.rb
@@ -4,8 +4,8 @@ class ProgressSummary
 
   def initialize(enrollments, options = {})
     @enrollments  = enrollments
-    @end_date     = options.fetch(:end_date, Date.today)
-    @start_date   = options.fetch(:start_date, enrollments.minimum(:created_at))
+    @end_date     = as_date(options.fetch(:end_date, Date.today))
+    @start_date   = as_date(options.fetch(:start_date, enrollments.minimum(:created_at)))
   end
 
   def to_a
@@ -15,9 +15,25 @@ class ProgressSummary
   end
 
   def summarize_progress_for_enrollment(enrollment)
-    { course_name: "sucka!", progress: {} }
+    { course_name: enrollment.course.name, progress: progress_burndown(enrollment) }
   end
   private :summarize_progress_for_enrollment
+
+  def as_date(val)
+    return val if val.kind_of? Date
+    return val.to_date if val.respond_to? :to_date
+    return Date.parse(val) if val.kind_of? String
+    raise InvalidArgumentError
+  end
+  private :as_date
+
+  def progress_burndown(enrollment)
+    skills_left = enrollment.course.skills.count
+    start_date.upto(end_date).each_with_object({}) do |date, hash|
+      hash[date] = skills_left
+    end
+  end
+  private :progress_burndown
 
   #
   # [
