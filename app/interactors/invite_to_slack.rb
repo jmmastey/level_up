0e1617721e4d45c207758bc639fb4140a048afc7
Joseph Mastey <jmmastey@gmail.com>
2015-05-18 12:10:21 -0500

    add slack channel email

---
+++ app/interactors/invite_to_slack.rb
@@ -0,0 +1,22 @@
+class InviteToSlack
+  include Interactor
+
+  def call
+    context.users.each { |user| invite_to_slack(user) }
+  end
+
+  private
+
+  def invite_to_slack(user)
+    update(user) && remind(user)
+  end
+
+  def update(user)
+    !user.slack_invite_sent_at &&
+      user.update_attributes!(slack_invite_sent_at: Time.now)
+  end
+
+  def remind(user)
+    UserMailer.slack_reminder(user).deliver_now
+  end
+end
