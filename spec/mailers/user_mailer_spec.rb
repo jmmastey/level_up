5610ade18645469a44ee9cf1ea6b295fe61efbae
Joseph Mastey <jmmastey@gmail.com>
2015-05-19 13:14:06 -0500

    adds references to slack invite to all outgoing emails. spam that shit.

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,10 +1,12 @@
 require "spec_helper"
 
 describe UserMailer do
-  let(:user) { build(:user) }
+  let(:enrollment) { build(:enrollment) }
+  let(:user) { enrollment.user }
+  let(:course) { enrollment.course }
+  let(:slack_invite_url) { "https://levelupslackinvites.herokuapp.com/" }
 
   describe "#confirm_enrollment" do
-    let(:course) { create(:course) }
     let(:mail) { UserMailer.confirm_enrollment(user, course) }
 
     it "confirms course enrollment" do
@@ -15,6 +17,10 @@ describe UserMailer do
     it "actually sends the email" do
       expect { mail.deliver_now! }.to change { deliveries }.by(1)
     end
+
+    it "includes some helpful links" do
+      expect(mail.body).to include(slack_invite_url)
+    end
   end
 
   describe "#slack_reminder" do
@@ -22,7 +28,31 @@ describe UserMailer do
 
     it "includes the slack invite link" do
       expect(mail.to).to include(user.email)
-      expect(mail.body).to include("https://levelupslackinvites.herokuapp.com/")
+      expect(mail.body).to include(slack_invite_url)
+    end
+
+    it "actually sends the email" do
+      expect { mail.deliver_now! }.to change { deliveries }.by(1)
+    end
+  end
+
+  describe "#activity_reminder" do
+    let(:mail) { UserMailer.activity_reminder(enrollment) }
+
+    it "includes some helpful links" do
+      expect(mail.body).to include(slack_invite_url)
+    end
+
+    it "actually sends the email" do
+      expect { mail.deliver_now! }.to change { deliveries }.by(1)
+    end
+  end
+
+  describe "#reg_reminder" do
+    let(:mail) { UserMailer.reg_reminder(user) }
+
+    it "includes some helpful links" do
+      expect(mail.body).to include(slack_invite_url)
     end
 
     it "actually sends the email" do
