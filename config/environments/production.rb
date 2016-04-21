bd007cea19486a24567cb3d719228a015a782b40
Joseph Mastey <jmmastey@gmail.com>
2016-04-21 15:13:22 -0500

    Replace Mandrill with SendGrid.
    Turns out Mandrill is turning off their integration and making it more
    of a pain anyway, so switch to another addon in Heroku.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,13 +77,14 @@ Levelup::Application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.mandrillapp.com",
+    from: "Joe @ LevelUpRails <jmmastey@gmail.com>",
+    address: "smtp.sendgrid.net",
     port: 587,
     domain: ENV["DOMAIN_NAME"],
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: ENV["MANDRILL_USERNAME"],
-    password: ENV["MANDRILL_APIKEY"],
+    user_name: ENV["SENDGRID_USERNAME"],
+    password: ENV["SENDGRID_PASSWORD"],
   }
   # ActionMailer Config
   config.action_mailer.default_url_options = { host: ENV["DOMAIN_NAME"] }
