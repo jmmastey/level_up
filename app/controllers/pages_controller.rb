995fdf5fed66b7a0dd60fd130b2fd3e29537c3e9
Joseph Mastey <jmmastey@gmail.com>
2016-09-01 13:40:20 +0200

    chore(certs): even more tries

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   # GET /.well-known/acme-challenge/:id
   def letsencrypt
-    render text: "uuonm85LZUwvqH0todE2UQCbYNmJ_XrHXL9W0HxoPZU.s4NILCReuafy1VQbGsfmxAnpU_2CSuP624LPg4Q6eK0"
+    render text: ENV['LETS_ENCRYPT_CODE']
   end
 end
