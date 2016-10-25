d8897d1757f99b3c88ed432e375d9758538f8eb4
Joseph Mastey <jmmastey@gmail.com>
2016-10-24 22:50:06 -0500

    Try again for asset host, but make it configurable without deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Levelup::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = "d1okufamzdwg47.cloudfront.net"
+  config.action_controller.asset_host = ENV.fetch('CDN_HOST', nil)
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are
